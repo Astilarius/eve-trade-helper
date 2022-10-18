a00ef45c01451eddac13e2f38d7ec4f5fb6410c1
Astilarius <ruslanpronko1@gmail.com>
2022-10-18 18:05:27 +0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# eve-trade-helper
-IN PROGRESS
-This Website helps players to trade in New Eden by calculating what items to buy from player's current location and sell them at other location to gain most profit
--- src/dataProcessing/setRoute.js
+++
@@ -1,8 +0,0 @@
-//sets route in users interface
-export function setRoute(token, dest){
-    fetch(`https://esi.evetech.net/latest/ui/autopilot/waypoint/?add_to_beginning=false&clear_other_waypoints=false&datasource=tranquility&destination_id=${dest}&token=${token}`,{
-        method: 'post',
-    });
-  }
-
-// export default setRoute
